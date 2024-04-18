012571e0d511cd0e04eb933fb8e044f9ed6a867d
ashokhope <167462698+ashokhope@users.noreply.github.com>
2024-04-18 23:03:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+var length = document.querySelectorAll(".drum").length;
+for(var i = 0 ; i< length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function() {
+       makesound(this.innerHTML);
+       btnanime(this.innerHTML);
+    })
+}
+ document.addEventListener("keypress" ,function(event){
+    makesound(event.key);
+    btnanime(event.key);
+ })
+ function makesound(key)
+ {
+    switch(key
+    ){
+        case "w":
+            var tom = new Audio('tom-1.mp3');
+            tom.play();
+            break;
+        case "a":
+            var tom = new Audio('tom-2.mp3');
+            tom.play();
+            break; 
+        case "s":
+            var tom = new Audio('tom-3.mp3');
+            tom.play();
+            break;     
+        case "d":
+            var tom = new Audio('tom-4.mp3');
+            tom.play();
+            break;
+        case "j":
+            var tom = new Audio('snare.mp3');
+            tom.play();
+            break;
+        case "k":
+            var tom = new Audio('crash.mp3');
+            tom.play();
+            break; 
+        case "l":
+            var tom = new Audio('kick-bass.mp3');
+            tom.play();
+            break;         
+    }
+ }
+ function btnanime(btn){
+    var activebutton= document.querySelector("."+btn);
+    activebutton.classList.add("pressed");
+    setTimeout(
+        function(){
+            activebutton.classList.remove("pressed")
+        },100
+    ) ;
+ }
